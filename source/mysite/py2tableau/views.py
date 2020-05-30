f62f11c18a77c47546c04e0f4f816dd1a56aea0c
unknown <femioho@gmail.com>
2020-05-30 18:58:13 -0400

    returning template

--- source/mysite/py2tableau/views.py
+++ source/mysite/py2tableau/views.py
@@ -3,4 +3,5 @@ from django.template import loader
 
 def home(request):
     template = loader.get_template('py2tableau/home.html')
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    return HttpResponse(template.render(request))
