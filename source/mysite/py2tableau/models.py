93d78d8039152af87025ac37fd14fdef30f5ee67
unknown <femioho@gmail.com>
2020-05-30 17:09:34 -0400

    started defining models

--- source/mysite/py2tableau/models.py
+++ source/mysite/py2tableau/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Covid(models.Model):
+    state = models.CharField(max_length=20)
+
+class User(models.Model):
+    Fname = models.CharField(max_length=20)
+    Lname = models.CharField(max_length=20)
