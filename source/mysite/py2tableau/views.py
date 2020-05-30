cc3b0ccc9c684a5dc25d98a2b2d57ff2f22b7680
unknown <femioho@gmail.com>
2020-05-30 18:57:03 -0400

    added reference to home.html template

--- source/mysite/py2tableau/views.py
+++ source/mysite/py2tableau/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
 
-
-def index(request):
+def home(request):
+    template = loader.get_template('py2tableau/home.html')
     return HttpResponse("Hello, world. You're at the polls index.")
