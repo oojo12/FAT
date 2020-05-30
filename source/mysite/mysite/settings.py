ca841064e1481ea41529ca76ccda9002420ea64d
unknown <femioho@gmail.com>
2020-05-30 17:16:18 -0400

    updated installed apps

--- source/mysite/mysite/settings.py
+++ source/mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'py2tableau.apps.py2tableau'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
