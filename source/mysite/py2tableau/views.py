ba4a350363b34d1a47346f65f4026ae3073cf95b
unknown <femioho@gmail.com>
2020-05-30 20:16:19 -0400

    clean up and returning user table on get request

--- source/mysite/py2tableau/views.py
+++ source/mysite/py2tableau/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 def home(request):
     return render(request, 'py2tableau/home.html')
+
+
+def update_user_db(request):
+    if request.method == "POST":
+        user = User(Fname=fname, Lname=lname)
+        user.save()
+    else:
+        return HttpResponse(User.objects.all())
