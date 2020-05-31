9808d849fc5f341a04de3759945d2adfbd2d01ff
unknown <femioho@gmail.com>
2020-05-30 22:41:52 -0400

    simplified and started to use Django forms

--- source/mysite/py2tableau/views.py
+++ source/mysite/py2tableau/views.py
@@ -1,14 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import *
-
+from .forms import *
 def home(request):
-    return render(request, 'py2tableau/home.html')
-
+    return HttpResponse("This is the home page")
 
 def update_user_db(request):
+    form = UserForm()
     if request.method == "POST":
-        user = User(Fname=fname, Lname=lname)
-        user.save()
-    else:
-        return HttpResponse(User.objects.all())
+        form = UserForm(request.POST)
+        if form.is_valid():
+            context = {
+                "form": form
+                }
+            User.objects.create(**form.cleaned_data)
+
+    return render(request, 'py2tableau/update_user.html')
+
+def check_user_db(request):
+    return HttpResponse(User.objects.all())
