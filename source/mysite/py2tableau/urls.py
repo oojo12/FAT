933f67e04a4c6b8ccc30caa178d2a8591f7e0e50
unknown <femioho@gmail.com>
2020-05-31 08:56:10 -0400

    added tableau to url patterns

--- source/mysite/py2tableau/urls.py
+++ source/mysite/py2tableau/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('update_user_db', views.update_user_db, name='update_user_db'),
+    path('tableau', views.tableau, name='tableau'),
     path('check_user_db', views.check_user_db, name='check_user_db')
 ]
