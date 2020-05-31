140e4de70daaca1dcc4c3223b426f44b7454b21c
unknown <femioho@gmail.com>
2020-05-30 20:15:11 -0400

    clean up

--- source/mysite/py2tableau/urls.py
+++ source/mysite/py2tableau/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/update_user_db', views.update_user_db, name='update_user_db')
+    path('update_user_db', views.update_user_db, name='update_user_db'),
+    path('view_user_data', views.view_user_data, name='view_user_data'),
 ]
