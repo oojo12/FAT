d0f26fd1643b9d6b0417e54ed773e4d41f3425c0
unknown <femioho@gmail.com>
2020-05-31 08:50:37 -0400

    minor bug fixes

--- source/mysite/py2tableau/views.py
+++ source/mysite/py2tableau/views.py
@@ -6,16 +6,16 @@ def home(request):
     return HttpResponse("This is the home page")
 
 def update_user_db(request):
-    form = UserForm()
+    form = UserForm(None)
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            context = {
-                "form": form
-                }
             User.objects.create(**form.cleaned_data)
+    context = {
+        "form": form
+        }
 
-    return render(request, 'py2tableau/update_user.html')
+    return render(request, 'py2tableau/update_user.html', context)
 
 def check_user_db(request):
     return HttpResponse(User.objects.all())
